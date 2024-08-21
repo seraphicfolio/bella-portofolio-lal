5c5ca6e16aea8ecf6f4e668f2e6493e32cf3c312
NEIR-0 <aalijlal.ganteng@gmail.com>
2024-08-21 10:38:05 +0700

    fix: revisi - 1

--- src/components/pages/homepages.jsx
+++ src/components/pages/homepages.jsx
@@ -6,7 +6,7 @@ export default function Homepages() {
     <>
       <section id="homepages" className="w-full h-[450px] md:h-[700px] lg:h-screen bg-[#FFEBD4] flex items-center justify-center px-5 md:px-[90px] lg:ps-[150px]">
         <div className="w-full flex flex-col items-start justify-start">
-            <h1 className="text-[25px] md:text-[40px] lg:text-[50px] font-light">I'M <span className="underline decoration-[#987070]">BELLA</span> AND I'M A</h1>
+            <h1 className="text-[25px] md:text-[40px] lg:text-[50px] font-light">I'M <span className="underline decoration-[#987070]">SHAFA</span> AND I'M A</h1>
             <div className="flex md:flex-row mb-5 lg:mb-10 lg:-mt-5">
               <ReactTyped className="text-[20px] md:text-[40px] lg:text-[100px]" strings={["user interface (UI)", "user experience (UX)"]} typeSpeed={40} backSpeed={50} loop />
               <h1 className="text-[20px] md:text-[40px] lg:text-[100px] font-semibold ms-1 md:ms-2">Designer</h1>
